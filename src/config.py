9aa08b842f755853cd056d8a66930866fd868600
Mojtaba Eshghie <eshghie@kth.se>
2024-07-06 15:24:33 +0200

    fix: config file path was incorrectly written in config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,10 @@
+import os
 import yaml
 
 class Config:
-    def __init__(self, config_file='config.yaml'):
+    def __init__(self, config_file=None):
+        if config_file is None:
+            config_file = os.path.join(os.path.dirname(__file__), '../config.yml')
         with open(config_file, 'r') as file:
             self.config = yaml.safe_load(file)
 
