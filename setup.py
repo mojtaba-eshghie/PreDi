036728026057c0a460e7ecc05df530009f2b607e
Mojtaba Eshghie <eshghie@kth.se>
2024-07-09 11:26:05 +0200

    Preparing the setup.py for installing the tool using pip

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SPi',
+    version='0.1.0',
+    author='Mojtaba Eshghie',
+    author_email='eshghie@kth.se',
+    description='SPi (Solidity Predicate Inspector): Solidity Semantic Predicate Comparison Tool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mojtaba-eshghie/spi',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # List your project's dependencies here.
+        # They will be installed by pip when your project is installed.
+        'sympy>=1.13.0rc2',
+        'colorama>=0.4.6',
+        'pyyaml>=6.0.1'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
